f2545139f729bcb0cdc2a187f91bd8ff696db727
Colby Miller <colbymillerdev@gmail.com>
2020-02-02 02:53:12 -0500

    Added check to only delete tweets before specified date. Added basic logging.

--- src/commands/tweets.js
+++ src/commands/tweets.js
@@ -1,11 +1,14 @@
 const { Command, flags } = require('@oclif/command');
 const fs = require('fs');
+const moment = require('moment');
 
 class TweetCommand extends Command {
   async run() {
     const { flags } = this.parse(TweetCommand);
-    const date = flags.date;
+    const inputDate = new Date(flags.date);
     let deleteCount = 0;
+    let retweetCount = 0;
+    let afterDateCount = 0;
 
     // Read in file and replace assignment to create parsed JSON.
     // TODO: Change back to tweet.js
@@ -13,20 +16,38 @@ class TweetCommand extends Command {
     originalFile = originalFile.replace('window.YTD.tweet.part0 = ', '');
     const tweets = JSON.parse(originalFile);
 
+    // TODO: Add in user friendly prompts to gather information.
+    // TODO: Setup Twitter API connections and keys.
     try {
       tweets.forEach(({ tweet }) => {
-        // Check for retweet, don't delete.
-        if (tweet.full_text.startsWith('RT')) return console.log('RETWEET');
+        // If tweet date is after the input date, don't delete.
+        const tweetDate = new Date(tweet.created_at);
+        if (moment(tweetDate).isAfter(moment(inputDate))) {
+          afterDateCount += 1;
+          return;
+        }
 
-        console.log(tweet.id);
+        // If retweet, don't attempt delete.
+        if (tweet.full_text.startsWith('RT')) {
+          retweetCount += 1;
+          return;
+        }
+
+        // TODO: Make call to Twitter API to delete tweet.
+        console.log(`Deteting tweet with ID = ${tweet.id}`);
+        deleteCount += 1;
+        return;
       });
     } catch (e) {
+      console.log(e);
       this.error(
         'It is likely Twitter has updated the JSON structure of tweet.js. Please create an issue at https://github.com/colbymillerdev/tweet-delete/issues so tweet-delete can be updated :)'
       );
     }
 
-    this.log(`Delete all tweets on and before ${date}`);
+    this.log(`${deleteCount} tweets were successfully deleted.`);
+    this.log(`${afterDateCount} tweets were after the specified date and NOT deleted.`);
+    this.log(`${retweetCount} tweets were retweeted from other users and NOT deleted.`);
   }
 }
 
